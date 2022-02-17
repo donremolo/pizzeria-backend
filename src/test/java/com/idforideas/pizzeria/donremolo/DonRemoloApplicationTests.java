73b3ad5f6be53883014d0076d3281142b5b04feb
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-02-17 19:08:32 -0400

    initializr spring boot project

---
+++ src/test/java/com/idforideas/pizzeria/donremolo/DonRemoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idforideas.pizzeria.donremolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonRemoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
